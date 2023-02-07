e342292f1e320849eebd0c2b6ea5a9dc6dc779d0
dz115 <dz115@student.le.ac.uk>
2023-02-07 14:59:42 +0000

    added navbar and reuse page

---
+++ RefugEAP/frontend/src/components/MenuItems.js
@@ -0,0 +1,21 @@
+export const MenuItems = [
+    {
+        title:'Home',
+        url: '#',
+        cName: 'nav-links',
+        icon:"fa-solid fa-house-user"
+    },
+    {
+        title:'Reuse',
+        url: '#',
+        cName: 'nav-links',
+        icon:"fa-solid fa-circle-info"
+    },
+    {
+        title:'About us',
+        url: '#',
+        cName: 'nav-links',
+        icon:"fa-solid fa-address-book"
+    },
+
+]
---
+++ RefugEAP/frontend/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import "./Navbar.css";
+import { MenuItems } from './MenuItems';
+
+
+class Navbar extends Component {
+    render(){
+        return(
+            <nav className='NavbarItems'>
+                <h1 className='logo'> React <img className='Clogo' src ="/src/images/logo.png"/>
+                </h1>
+                <ul className='bar'>
+                    {MenuItems.map((item,index)=>{
+                        return(
+                        <li key={index}>
+                            <a href={item.url}
+                            className={item.cName} >
+                                <i className={item.icon}>
+                                    {item.title}
+                                </i>
+                            </a>
+                        </li>
+
+                        )
+                    })}
+                    
+                </ul>
+        </nav>
+
+        )
+    }
+}
+
+
+
+
+export default Navbar;
