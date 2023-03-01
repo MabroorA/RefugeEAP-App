7969f3adea74dbafddbe99f1b7019142e24b382c
swtcoding <swtcoding@gmail.com>
2023-03-01 11:13:01 +0000

    calendar css

--- RefugEAP/frontend/src/components/Calendar.js
+++ RefugEAP/frontend/src/components/Calendar.js
@@ -1,6 +1,6 @@
 import { Scrollbars } from "react-custom-scrollbars";
 import React, { useState, useEffect } from "react";
-import {Calendar, dateFnsLocalizer} from "react-big-calendar";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 // import './Calendar;.css';
 import format from "date-fns/format";
 import parse from "date-fns/parse";
@@ -11,42 +11,42 @@ import DatePicker from "react-datepicker";
 /* import {events} from '../data'; */
 
 const locales = {
-  "en-GB": require('date-fns/locale/en-GB')
-}
+  "en-GB": require("date-fns/locale/en-GB"),
+};
 const localizer = dateFnsLocalizer({
   format,
   parse,
   startOfWeek,
   getDay,
-  locales
-})
+  locales,
+});
 
 const events = [
   {
     title: "MEETING1",
     allday: true,
     start: new Date(2023, 1, 3),
-    end: new Date(2023, 1, 4)
+    end: new Date(2023, 1, 4),
   },
 
   {
     title: "MEETING2",
     start: new Date(2023, 2, 3),
-    end: new Date(2023, 2, 4)
+    end: new Date(2023, 2, 4),
   },
 
   {
     title: "MEETING3",
     start: new Date(2023, 2, 3),
-    end: new Date(2023, 2, 4)
+    end: new Date(2023, 2, 4),
   },
 
   {
     title: "MEETING4",
     start: new Date(2023, 2, 3),
-    end: new Date(2023, 2, 4)
-  }
-]
+    end: new Date(2023, 2, 4),
+  },
+];
 
 function Calendar2() {
   const [data, setData] = useState([]);
@@ -58,39 +58,39 @@ function Calendar2() {
       .then((response) => setData(response.data));
   }, []);
 
-  const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""})
-  const [allEvents, setAllEvent] = useState(events)
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvent] = useState(events);
 
-    function handleAddEvent() {
-      setAllEvent([...allEvents, newEvent])
-    }
+  function handleAddEvent() {
+    setAllEvent([...allEvents, newEvent]);
+  }
 
-
-    return (
-    <section className="section" id="event">
-      <div className="section-title">
-        <h2>Events</h2>
+  return (
+    <>
+      <div>
+        <p>Add New Event</p>
       </div>
       <div>
-      <p>Add New Event</p>
-      </div>
-      <div>
-        <input type="text" placeholder="Add Title" style={{width: "20%", marginRight:"10px"}} 
-        value={newEvent.title} onChange = {(e) => setNewEvent({... newEvent, title: e.target.value})}  //This part adds in the title for the new event idk if you want this to be changed also
+        <input
+          type="text"
+          placeholder="Add Title"
+          style={{ width: "20%", marginRight: "10px" }}
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} //This part adds in the title for the new event idk if you want this to be changed also
         />
       </div>
       <div>
         <div className="event-calendar">
-          <Calendar 
+          <Calendar
             localizer={localizer}
-            events={allEvents} 
-            startAccessor="start" 
-            endAccessor="end" 
-            style={{height: 400, margin: "50px", zIndex: 0}}    //Had an issue with event calender overlapping the navbar added to the navbar.css z-index: 2
+            events={allEvents}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 400, margin: "50px", zIndex: 0 }} //Had an issue with event calender overlapping the navbar added to the navbar.css z-index: 2
           />
         </div>
       </div>
-    </section>
+    </>
   );
 }
 
