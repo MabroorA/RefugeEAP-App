3fd9e66aec0f7457d36f020e9dee4c94b9f1a593
swtcoding <swtcoding@gmail.com>
2023-03-13 16:07:16 +0000

    pagination done and fetch blogs from db

--- RefugEAP/frontend/src/components/Blogs.js
+++ RefugEAP/frontend/src/components/Blogs.js
@@ -1,20 +1,68 @@
-import React from 'react';
-import BlogCard from './BlogCard';
-import Pagination from "react-bootstrap/Pagination";
+import React, { useState, useEffect } from "react";
+import BlogCard from "./BlogCard";
+import { useFetch } from "./useFetch";
 
-const Blogs = ({props}) => {
-  const numOfBlogsPerPage = 5;
-  
+const Blogs = () => {
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setBlogs(data[page]);
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage + 1;
+      if (prevPage < 0) {
+        nextPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
   return (
     <>
-      {props?.map((blog) => {
-        return (
-            <BlogCard blog={blog} />
-        ); 
+      {blogs?.map((blog) => {
+        return <BlogCard blog={blog} />;
       })}
+      {!loading && (
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPage}>
+            prev
+          </button>
+          {data.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={`page-btn ${index === page ? "active-btn" : null}`}
+                onClick={() => handlePage(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+          <button className="next-btn" onClick={nextPage}>
+            next
+          </button>
+        </div>
+      )}
     </>
-
   );
-}
+};
 
 export default Blogs;
--- RefugEAP/frontend/src/pages/JoinUs.js
+++ RefugEAP/frontend/src/pages/JoinUs.js
@@ -1,36 +1,88 @@
-import './css/JoinUs.css';
-import React from 'react';
+import "./css/JoinUs.css";
+import React from "react";
 
 function JoinUs() {
   return (
-        <div id="page">
+    <div id="page">
       <div id="title">
         <h1>Join us</h1>
       </div>
       <div id="interest">
-        <p>If you are keen to get involved as an active member of the RefugEAP Network Working Group (e.g. working to help us build content for the website) or perhaps a volunteer tutor on the RefugEAP Programme, you can fill in this online expression of interest form</p>
-        <button class="mainButton"><a class="link" href="https://docs.google.com/forms/d/e/1FAIpQLSfy_N8cqvLV2FGPrfWBXR-W64asV0yz4AqdHwDTCiOmH93P7Q/viewform">LEARN MORE</a></button>
+        <p>
+          If you are keen to get involved as an active member of the RefugEAP
+          Network Working Group (e.g. working to help us build content for the
+          website) or perhaps a volunteer tutor on the RefugEAP Programme, you
+          can fill in this online expression of interest form
+        </p>
+        <button class="mainButton">
+          <a
+            class="link"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfy_N8cqvLV2FGPrfWBXR-W64asV0yz4AqdHwDTCiOmH93P7Q/viewform"
+          >
+            LEARN MORE
+          </a>
+        </button>
       </div>
       <div id="mailing">
-        <p>Join the RefugEAP Network by subscribing to our JISCmail discussion list. This functions as a mutual support network for individuals and organisations involved and/or interested in developing widening participation initiatives for refugee-background students to facilitate access to HE via English language pathways, with a particular focus on English for Academic Purposes. It will also be a place where you can keep up to date with what’s happening in the Working Group and on the RefugEAP Programme (which will sometimes put out a call for more volunteer tutors via this JISCmail list)</p>
-        <button class="mainButton"><a class="link" href="https://www.jiscmail.ac.uk/cgi-bin/webadmin?A0=SJ-SIG-BALEAP">LEARN MORE</a></button>
+        <p>
+          Join the RefugEAP Network by subscribing to our JISCmail discussion
+          list. This functions as a mutual support network for individuals and
+          organisations involved and/or interested in developing widening
+          participation initiatives for refugee-background students to
+          facilitate access to HE via English language pathways, with a
+          particular focus on English for Academic Purposes. It will also be a
+          place where you can keep up to date with what’s happening in the
+          Working Group and on the RefugEAP Programme (which will sometimes put
+          out a call for more volunteer tutors via this JISCmail list)
+        </p>
+        <button class="mainButton">
+          <a
+            class="link"
+            href="https://www.jiscmail.ac.uk/cgi-bin/webadmin?A0=SJ-SIG-BALEAP"
+          >
+            LEARN MORE
+          </a>
+        </button>
       </div>
       <div>
-        <p>You could contribute to our RefugEAP Network Resource Bank Padlet. In particular, we are looking for people to share any relevant sanctuary-related job/role descriptions, so that we can begin to build a bank of these to share. Similarly, if you notice any category of resources missing, please let us know and we can then add it</p>
+        <p>
+          You could contribute to our RefugEAP Network Resource Bank Padlet. In
+          particular, we are looking for people to share any relevant
+          sanctuary-related job/role descriptions, so that we can begin to build
+          a bank of these to share. Similarly, if you notice any category of
+          resources missing, please let us know and we can then add it
+        </p>
       </div>
       <div>
-        <p>Contribute a post to our website via the Blog. This might be in the form of a case study, a testimonial, or another type of contribution</p>
+        <p>
+          Contribute a post to our website via the Blog. This might be in the
+          form of a case study, a testimonial, or another type of contribution
+        </p>
       </div>
       <div id="UniMailList">
-        <p>Other similar networks which are not EAP-specific but which are very useful for those working in EAP/HE contexts are the Universities of Sanctuary JISCmail discussion list, and the Hub for European Refugee Education mailing list</p>
-        <button class="mainButton"><a class="link" href="https://jiscmail.ac.uk/cgi-bin/webadmin?SUBED1=SANCTUARY&A=1">LEARN MORE</a></button>
+        <p>
+          Other similar networks which are not EAP-specific but which are very
+          useful for those working in EAP/HE contexts are the Universities of
+          Sanctuary JISCmail discussion list, and the Hub for European Refugee
+          Education mailing list
+        </p>
+        <button class="mainButton">
+          <a
+            class="link"
+            href="https://jiscmail.ac.uk/cgi-bin/webadmin?SUBED1=SANCTUARY&A=1"
+          >
+            LEARN MORE
+          </a>
+        </button>
       </div>
       <div>
-        <p>For any other queries, please contact RefugEAP Network Convenor Aleks Palanac at ap417@le.ac.uk</p>
+        <p>
+          For any other queries, please contact RefugEAP Network Convenor Aleks
+          Palanac at ap417@le.ac.uk
+        </p>
       </div>
     </div>
   );
 }
-
 
 export default JoinUs;
