91734a37284ea6ee6fd514fb98286d56312a5883
swtcoding <swtcoding@gmail.com>
2023-03-13 16:08:10 +0000

    deleted unneccessary code

--- RefugEAP/pages/urls.py
+++ RefugEAP/pages/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.ReactEventView, name='ReactEventView')
-#     path('about/', AboutPageView.as_view(), name='about'),
-#     path('', HomePageView.as_view(), name='home')
 ]
