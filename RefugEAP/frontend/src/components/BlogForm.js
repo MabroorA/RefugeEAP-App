0785bcee27af4155f943dc79c4839ed79b82e43c
swtcoding <swtcoding@gmail.com>
2023-03-14 12:33:04 +0000

    Blog Form updated

--- RefugEAP/frontend/src/components/BlogForm.js
+++ RefugEAP/frontend/src/components/BlogForm.js
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+const BlogForm = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [customValue, setCustomValue] = useState("");
 
-function BlogForm () {
-  const [selectedOption, setSelectedOption] = useState('');
-  const [customValue, setCustomValue] = useState('');
-
-  const handleSelectChange = (event) => {
-    setSelectedOption(event.target.value);
-    setCustomValue('');
+  const handleSelectChange = () => {
+    setSelectedOption();
+    setCustomValue("");
   };
 
-  const handleCustomInputChange = (event) => {
-    setSelectedOption('custom');
-    setCustomValue(event.target.value);
+  const handleCustomInputChange = () => {
+    setSelectedOption("custom");
+    setCustomValue();
   };
-
   return (
     <Form>
       <Form.Group className="mb-3" controlId="Fullname">
         <Form.Label>Full name</Form.Label>
         <Form.Control type="text" placeholder="Enter Full name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="Affiliation">
         <Form.Label>Affiliation</Form.Label>
@@ -29,16 +31,16 @@ function BlogForm () {
       </Form.Group>
       <Form.Group controlId="formSelectRole">
         <Form.Label>Select a role or please specify other</Form.Label>
-        <Form.Control as="select" value={selectedOption} onChange={handleSelectChange}>
+        <Form.Control
+          as="select"
+          value={selectedOption}
+          onChange={() => handleSelectChange()}
+        >
           <option value="option1">Practitioner</option>
           <option value="option2">Student</option>
           <option value="option3">Research</option>
           <option value="option3">Other</option>
-        </Form.Control> 
-        {selectedOption === 'Other' && (
-          <Form.Control type="text" value ={customValue} onChange={handleCustomInputChange}
-          placeholder="Other option" />
-        ) }
+        </Form.Control>
       </Form.Group>
       <Form.Group controlId="formSelectcont">
         <Form.Label>Select type of Contribution</Form.Label>
@@ -47,6 +49,14 @@ function BlogForm () {
           <option value="option2">Testimonial</option>
           <option value="option3">Other</option>
         </Form.Control>
+        {selectedOption === "Other" && (
+          <Form.Control
+            type="text"
+            value={customValue}
+            onChange={() => handleCustomInputChange()}
+            placeholder="Other option"
+          />
+        )}
       </Form.Group>
       <Form.Group className="mb-3" controlId="ContributionTitle">
         <Form.Label>Contribution Title</Form.Label>
@@ -56,20 +66,9 @@ function BlogForm () {
         <Form.Label>Introductory blurb</Form.Label>
         <Form.Control type="text" placeholder="Enter an introductory blurb" />
       </Form.Group>
-      <Form.Group controlId="formContent">
-        <Form.Label>Content</Form.Label>
-        <Form.Control as="text" rows={3} placeholder="Enter content or upload files below" />
-        <Form.File id="custom-file" label="Choose file" custom />
-      </Form.Group>
-
-
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group controlId="formFile" className="mb-3">
+        <Form.Label>Default file input example</Form.Label>
+        <Form.Control type="file" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
@@ -79,6 +78,6 @@ function BlogForm () {
       </Button>
     </Form>
   );
-}
+};
 
-export default BlogForm
+export default BlogForm;
