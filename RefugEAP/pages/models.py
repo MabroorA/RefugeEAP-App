2fae99c4f43200016553f86050dd9cc481b1baae
swtcoding <swtcoding@gmail.com>
2023-03-14 19:23:02 +0000

    pages table updated

--- RefugEAP/pages/models.py
+++ RefugEAP/pages/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=150)
     date = models.DateField()
-    time = models.CharField(max_length=10)
-    more = models.URLField()
+    time = models.CharField(max_length=20)
+    more = models.TextField()
+    link = models.URLField(null=True)
 
     def __str__(self):
         return self.name
+
