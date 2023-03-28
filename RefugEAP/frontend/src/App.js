cd0de9006d182f774775ecfcaee8a916553e3995
swtcoding <swtcoding@gmail.com>
2023-03-28 01:17:55 +0100

    Route for admin dashboard

--- RefugEAP/frontend/src/App.js
+++ RefugEAP/frontend/src/App.js
@@ -12,24 +12,46 @@ import ContactUs from "./pages/ContactUs";
 import BlogPage from "./pages/BlogPage";
 import JoinUs from "./pages/JoinUs";
 import BlogSubmission from "./pages/BlogSubmission";
-
+import { SharedLayout } from "./pages/SharedLayout";
+import { BlogLayout } from "./pages/BlogLayout";
+import Dashboard from "./pages/dashboard/Dashboard";
+import AllBlogs from "./pages/dashboard/AllBlogs";
+import BaseLayout from "./pages/dashboard/BaseLayout";
+import AddBlog from "./pages/dashboard/AddBlog";
+import Login from "./pages/dashboard/Login";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="about" element={<AboutUsPage />} />
-        <Route path="refugeap" element={<EAPPage />} />
-        <Route path="join" element={<JoinUs />} />
-        <Route path="contactus" element={<ContactUs />} />
-        <Route path="Reuse" element={<Reuse />} />
-        <Route path="Guidelines" element={<Guidelines />} />
-        <Route path="blog" element={<BlogPage />} />
-        <Route path="form" element={<BlogSubmission />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="refugeap" element={<EAPPage />} />
+          <Route path="join" element={<JoinUs />} />
+          <Route path="contactus" element={<ContactUs />} />
+          <Route path="Reuse" element={<Reuse />} />
+          <Route path="Guidelines" element={<Guidelines />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="blog/:blogId" element={<BlogLayout />} />
+          <Route path="form" element={<BlogSubmission />} />
+        </Route>
+        <Route>
+          <Route
+            path="/dashboard"
+            element={
+              // <ProtectedRoute>
+              <BaseLayout />
+              // </ProtectedRoute>
+            }
+          >
+            <Route index element={<Dashboard />} />
+            <Route path="all-blogs" element={<AllBlogs />} />
+            <Route path="add-blog" element={<AddBlog />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
---
+++ RefugEAP/frontend/src/pages/BlogLayout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BlogLayout = () => {
+  return (
+    <h1>This is the blog layout</h1>
+  )
+}
